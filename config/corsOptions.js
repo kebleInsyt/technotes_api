85936157787e1e47e23a590152fcd3f7e3a74c54
kebleInsyt <k.chiemeka65@gmail.com>
2023-08-20 23:53:18 +0100

    adjusted corsoptions

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 ) //add !origin to allow access to REST apps like postman 
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin) //add !origin to allow access to REST apps like postman 
         {
             callback(null, true);
         } else {
