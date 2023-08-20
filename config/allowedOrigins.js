f47cd867afdcdc0f2545dbe43743e7eeb56927d6
kebleInsyt <k.chiemeka65@gmail.com>
2023-08-20 23:14:11 +0100

    amended the allowedOrigins and corsOptions files

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,11 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "https://www.dandrepairshop.com",
-  "https://dandrepairshop.com",
-  "https://google.com",
-  "https://www.google.com"
-];
+const allowedOrigins = ["https://techrepairs.onrender.com"];
 
 module.exports = allowedOrigins;
